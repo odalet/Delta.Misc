3db6d9c3553d4693ee037bc7c2488db3284e1de9
Olivier DALET <odalet@gmail.com>
2016-02-13 23:55:22 +0100

    sonar

--- TestCI/TestJenkinsSonarMSTest/build-sonar.cmd
+++ TestCI/TestJenkinsSonarMSTest/build-sonar.cmd
@@ -1,12 +1,12 @@
-set dir=%~dp0
+pushd
 cd %~dp0
 
 rd /q /s "%~dp0.sonarqube"
 
 C:\JAVA\JENKINS\tools\hudson.plugins.sonar.MsBuildSQRunnerInstallation\MSBuild_SornarQube_Runner_1.1\MSBuild.SonarQube.Runner.exe begin /k:TestJenkinsSonarMSTest /n:TestJenkinsSonarMSTest /v:1.0 /d:sonar.host.url=http://localhost:9000
 
-"C:\Program Files (x86)\MSBuild\14.0\Bin\amd64\MSBuild.exe" "%%~dp0TestJenkinsSonarMSTest.sln"
+"C:\Program Files (x86)\MSBuild\14.0\Bin\amd64\MSBuild.exe" "%~dp0.\TestJenkinsSonarMSTest.sln"
 
 C:\JAVA\JENKINS\tools\hudson.plugins.sonar.MsBuildSQRunnerInstallation\MSBuild_SornarQube_Runner_1.1\MSBuild.SonarQube.Runner.exe end
 
-cd %dir%
+popd
